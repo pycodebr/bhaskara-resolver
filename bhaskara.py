5bcd959da00763977ea86ad347030e75d8bfaba6
vitorstaub <87439827+vitorstaub@users.noreply.github.com>
2022-02-28 19:41:45 -0300

    add math and  if statements

--- bhaskara.py
+++ bhaskara.py
@@ -1,8 +1,15 @@
+import math
+
 def Bhaskara(a, b , c):
-    delta = (b ** 2) - (4 * a * c)
+    delta = (math.pow(b, 2)) - (4 * a * c)
     
-    x1 = (-b + delta ** (1 / 2)) / (2 * a)
-    x2 = (-b - delta ** (1 / 2)) / (2 * a)
+    if a == 0:
+        print('\nO valor de A deve ser diferente de 0')
+    elif delta < 0:
+        print('\nSem raizes reais')
+    else:
+        x1 = (-b + math.sqrt(delta)) / (2 * a)
+        x2 = (-b - math.sqrt(delta)) / (2 * a)
     
     print('\nValor de x1: {0}'.format(x1))
     print('Valor de x2: {0}'.format(x2))
